4faa55f4c8fb837b7bebe086d2473caef3f0c3de
bad06denby <35545034+bad06denby@users.noreply.github.com>
2018-01-22 13:25:41 -0800

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,13 @@
+door = int(input('''You go down the dark hallway and come across two doors.
+Do you go down door #1 or door #2? '''))
+
+if door == 1:
+    print("A monster jumps out and screams,'It's peanut butter jelly time!!!")
+
+elif door == 2:
+    print('''A sexy woman comes out and says,
+    'Would you like a rumpled fanny bottom?'
+    ''')
+
+else:
+    print("Thats not a door. It's a shitty answer to my question.")
